a4c1626ba70c0b8b93b86395e48f6b4fa166f042
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-08-02 20:48:27 +0530

    first commit

---
+++ src/main/java/com/MongoDB/configuration/MongoDbConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.MongoDB.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbConfigurationApplication.class, args);
+	}
+
+}
